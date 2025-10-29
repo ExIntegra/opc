31bd426f8ba2c141054be7a7155de1d4f99b5a80
exinterga <ilyakalinin00@yandex.ru>
2025-10-29 23:10:37 +0300

    Reapply "Создание отдельного модуря с инициализацией полей pid, sensor, valve."
    This reverts commit 4afcd15371f45163b9d753d81489b07183a9605b.

---
+++ opc_demo/init.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <open62541/types.h>
+#include "types.h"
+
+void cash_init(CashSensor* t);
+void valve_init(Valve* v);
+void sensor_init(Sensor* s);
+void pid_init(PIDControllerType* pid);
