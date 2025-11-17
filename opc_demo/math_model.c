19cf2e997e720604978e2cc825c0d08957431fd2
exinterga <ilyakalinin00@yandex.ru>
2025-11-17 17:54:32 +0300

    Перевод параметров в СИ.

--- opc_demo/math_model.c
+++ opc_demo/math_model.c
@@ -6,9 +6,7 @@ double compute_CB(Reactor reactor, Sensor sensorTemperature,
     ConfigMathModel config, Sensor sensorQ, Sensor sensorConcentrationA)
 {
     const double R = config.R;
-    printf("Получаю температуру...\n");
     const double T_K = sensorTemperature.io.pv + 273.15;
-    printf("Получил T: %.2f\n", sensorTemperature.io.pv);
     if (!isfinite(T_K) || T_K <= 0.0) {
         printf("Invalid temperature T=%.9f\n", T_K);
         return NAN;
@@ -16,7 +14,7 @@ double compute_CB(Reactor reactor, Sensor sensorTemperature,
 
 	const double Q = sensorQ.io.pv * 1e-3 / 60.0; // м3/с
     const double Vr = reactor.volume * 1e-3; // м3
-    const double CA = sensorConcentrationA.io.pv;
+    const double CA = sensorConcentrationA.io.pv; //перевести в си из моль/л
 
     const double k1 = (config.k01 / 60.0) * exp(-config.EA1 / (R * T_K));
     const double k2 = (config.k02 / 60.0) * exp(-config.EA2 / (R * T_K));
@@ -30,27 +28,33 @@ double compute_CB(Reactor reactor, Sensor sensorTemperature,
     }
 
     const double num = 2.0 * Vr * k1 * Q * CA;
-    printf("Результаты математической модели: \n");
-    printf("T=%.2f Q=%.2f Vr=%.2f CA=%.2f\n",
-		T_K, Q, Vr, CA);
-    printf("k1=%.9f k2=%.9f a=%.9f b=%.9f num=%.9f CB=%.12f\n",
-        k1, k2, a, b, num, num / (a * b));
-    printf("----------------------------------------------------------");
-	printf("\n\n\n");
-
-    return num / (a * b);
-}
+    printf("----------------------------------------------------------\n");
+        if (reactor.mixer) {
+            printf("Уставки для математической модели:\n\n");
+            printf("T=%.2f\nQ=%.2f\nVr=%.2f\nCA=%.2f\n\n", T_K, Q, Vr, CA);
+            printf("Результат математической модели:\n\n");
+            printf("k1=%.9f\nk2=%.9f\na=%.9f\nb=%.9f\nnum=%.9f\nCB=%.12f\n",
+                k1, k2, a, b, num, num / (a * b));
+            printf("----------------------------------------------------------\n");
+            return num / (a * b);
+        }
+        printf("Мешалка выключена, расчет мат. модели не произведен.\n");
+        printf("----------------------------------------------------------\n");
+        return 0;
+   }
 
 void model_cb(UA_Server* server, void* data) {
     (void)server;
+    printf("\nЗапуск математической модели:\n");
     ModelCtx* m = (ModelCtx*)data;
-    m->sensorConcentrationA->io.pv = (m->valveRegulationConcentrationA->manualoutput) / 100.0;
 	m->sensorF->io.pv = valve_characteristic(m->valveRegulationQ->manualoutput);
-    m->sensorConcentrationA->io.pv = valve_characteristicCA(m->valveRegulationConcentrationA->manualoutput);
+    m->sensorConcentrationA->io.pv = valve_characteristicCA(m->valveRegulationConcentrationA->manualoutput) * 1000;
     double y = compute_CB(*m->reactor, *m->sensorTemperature, m->cfg, *m->sensorF, *m->sensorConcentrationA);
 
     if (isfinite(y) && y >= 0.0)
         m->sensorConcentrationB->io.pv = y;
+    printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n\n\n");
+
 }
 
 static double valve_characteristic(double u) {
@@ -80,11 +84,11 @@ static double valve_characteristicCA(double u) {
         return 0.9;
 
     if (u <= 70.0) {
-        double x = u / 70.0;          // 0..1
+        double x = u / 70.0;
         return 0.7 * x * x;
     }
     else {
-        double x = (u - 70.0) / 30.0; // 0..1
+        double x = (u - 70.0) / 30.0;
         return 0.7 + 0.2 * x;
     }
 }
